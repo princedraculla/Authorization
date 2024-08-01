7753722bd4082444e84c6e72126e4fb4a097c3eb
princedraculla <amirtorkashvand56@gmail.com>
2024-08-02 00:33:27 +0330

    (decorator): require permissions

---
+++ src/decorators/require-permissions.decorator.ts
@@ -0,0 +1,9 @@
+import { SetMetadata } from '@nestjs/common';
+import { Action } from 'src/enums/action.enum';
+import { Subject } from 'src/enums/subject.enum';
+
+export const RequirePermissions = (
+  ...permissions: { action: Action; subject: Subject }[]
+) => {
+  SetMetadata('permissions', permissions);
+};
